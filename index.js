7ea793779989eedf81bf81dc894daaae27d20c2b
Yaniv Ben Hemo <yanivbenhemo@Yanivs-MBP-2.lan>
2021-03-10 11:17:23 +0200

    init

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3000
 
 app.post('/test', (req, res) => {
     console.log("Something arrived")
-    console.log(req)
+    console.log(req.originalUrl)
+    console.log(req.headers)
     console.log(req.body)
     res.status(200).json({
         status: 200,
