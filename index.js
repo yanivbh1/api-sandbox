18563448089ea3460a0bbdd6b7d8cea4c53e85c8
Yaniv Ben Hemo <yanivbenhemo@Yanivs-MBP-2.lan>
2021-03-10 11:02:15 +0200

    init

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
+    console.log("TEST")
     res.status(200).json({
         status: 200,
         message: `Hey`,
